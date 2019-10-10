2cec99fdcd231eb3c0fc39b1085d99f8e9af6f42
sgraaf <8904453+sgraaf@users.noreply.github.com>
2019-10-10 15:36:39 +0200

    Added the models to a seperate file

---
+++ models.py
@@ -0,0 +1,38 @@
+import torch
+from torch import nn
+
+class PolicyNetwork(nn.Module):
+    
+    def __init__(self, input_dim=2, hidden_dim=64, output_dim=4):
+        super(PolicyNetwork, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+
+        self.model = nn.Sequential(
+            nn.Linear(self.input_dim, self.hidden_dim),
+            nn.ReLU(),
+            nn.Linear(self.hidden_dim, self.output_dim),
+            nn.LogSoftmax(dim=0)
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class ValueNetwork(nn.Module):
+    
+    def __init__(self, input_dim=2, hidden_dim=64, output_dim=1):
+        super(ValueNetwork, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+
+        self.model = nn.Sequential(
+            nn.Linear(self.input_dim, self.hidden_dim),
+            nn.ReLU(),
+            nn.Linear(self.hidden_dim, self.output_dim)
+        )
+
+    def forward(self, x):
+        return self.model(x)
